8e81024452e9d29b56697d232df1c870ee166f6a
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-05-17 15:36:15 -0700

    completed max gain

--- maxgain.py
+++ maxgain.py
@@ -16,6 +16,10 @@ os.makedirs(old_folder_path, exist_ok=True)
 # Specify the column number to process (starting from 0)
 column_number = 0
 
+# Create variables to store the closest match in current CSV file
+frequency = None
+decibel = None
+
 while True:
     # Get the list of files in the folder
     files = os.listdir(folder_path)
@@ -26,7 +30,7 @@ while True:
     if not csv_files:
         print("No CSV files found in the folder.")
         # Wait for a specified duration before checking again
-        time.sleep(60)  # Adjust the duration as per your requirement
+        time.sleep(5)  # Adjust the duration as per your requirement
         continue
 
     # Find the most recent CSV file
@@ -35,9 +39,10 @@ while True:
     # Read the most recent CSV file
     with open(os.path.join(folder_path, most_recent_file), 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        rows = list(csv_reader)
 
         # Get user input and multiply by 1000000
-        user_input = input("Enter a number (or 'exit' to quit): ")
+        user_input = input("Enter frequency (in MHz) (or 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
 
@@ -46,30 +51,44 @@ while True:
         closest_diff = float('inf')
         closest_row = None
 
-        # Iterate through the rows and find the closest equivalent number in column 0
-        for row in csv_reader:
-            number_str = row[column_number].split(';')[0]  # Extract the number to the left of the semicolon
-            number = float(number_str)
+        # Find the closest match in the current CSV file
+        for row in rows:
+            number_str = row[column_number].split(';')[0]  # Extract the frequency
+            number = float(number_str) #convert user input as string to float
             diff = abs(user_number - number)
             if diff < closest_diff:
                 closest_diff = diff
                 closest_row = row
 
-    # Display the numbers to the left and right of the semicolon of the closest equivalent row
-    if closest_row is not None:
-        left_of_semicolon = closest_row[column_number].split(';')[0]
-        right_of_semicolon = closest_row[column_number].split(';')[1]
-        print(f"The number to the left of the semicolon of the closest equivalent row is: {left_of_semicolon}")
-        print(f"The number to the right of the semicolon of the closest equivalent row is: {right_of_semicolon}")
+        if closest_row is not None:
+            frequency = closest_row[column_number].split(';')[0]
+            decibel = closest_row[column_number].split(';')[1]
+            #test code, if broke, delete this
+            display_freq_in_MHz = float(frequency) / 1000000
+            print(f"The closest frequency in the current CSV file is: {display_freq_in_MHz} MHz with {decibel} dB")
 
-    # Ask the user if they want to move the CSV file to the old folder
-    move_file = input("Do you want to move the CSV file to the 'old_csv' folder? (y/n): ")
+    high_dB = float(decibel)
+    highest_file = None
 
-    if move_file.lower() == 'y':
-        # Move the most recent CSV file to the old folder
-        old_file_path = os.path.join(old_folder_path, most_recent_file)
-        shutil.move(os.path.join(folder_path, most_recent_file), old_file_path)
-        print(f"Moved {most_recent_file} to {old_folder_name} folder.")
+    # Iterate through the old CSV files
+    for old_file in os.listdir(old_folder_path):
+        old_file_path = os.path.join(old_folder_path, old_file)
+        with open(old_file_path, 'r') as old_csv_file:
+            old_csv_reader = csv.reader(old_csv_file, delimiter=',')
+
+            # Find the highest value in varB
+            for old_row in old_csv_reader:
+                old_left = old_row[column_number].split(';')[0]
+                old_right = old_row[column_number].split(';')[1]
+                if old_left == frequency:
+                    old_number = float(old_right)
+                    if old_number > high_dB:
+                        high_dB = old_number
+                        highest_file = old_file
+
+    if highest_file is not None:
+        display_freq_in_MHz_2 = float(frequency) / 1000000
+        print(f"The highest value in varB among old CSV files is: {display_freq_in_MHz_2} MHz at {high_dB} dB (found in {highest_file})")
 
     # Wait for a specified duration before checking again
     time.sleep(5)  # Adjust the duration as per your requirement
