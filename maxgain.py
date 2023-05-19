b77ab166656feed2c330ba48c0ae42f876b903bd
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-05-18 19:55:44 -0700

    Final draft fr fr

--- maxgain.py
+++ maxgain.py
@@ -28,10 +28,11 @@ while True:
     csv_files = [file for file in files if file.endswith('.csv')]
 
     if not csv_files:
-        print("No CSV files found in the folder.")
+        print("No CSV files found in the folder, shutting down program")
         # Wait for a specified duration before checking again
         time.sleep(5)  # Adjust the duration as per your requirement
-        continue
+        break
+        #continue
 
     # Find the most recent CSV file
     most_recent_file = max(csv_files, key=lambda x: os.path.getmtime(os.path.join(folder_path, x)))
@@ -63,7 +64,7 @@ while True:
         if closest_row is not None:
             frequency = closest_row[column_number].split(';')[0]
             decibel = closest_row[column_number].split(';')[1]
-            #test code, if broke, delete this
+            #test code, if broke, delete this but converts hz default to mhz
             display_freq_in_MHz = float(frequency) / 1000000
             print(f"The closest frequency in the current CSV file '{most_recent_file}' is: {display_freq_in_MHz} MHz with {decibel} dB")
 
@@ -76,7 +77,7 @@ while True:
         with open(old_file_path, 'r') as old_csv_file:
             old_csv_reader = csv.reader(old_csv_file, delimiter=',')
 
-            # Find the highest value in varB
+            # Find the highest decibel
             for old_row in old_csv_reader:
                 old_left = old_row[column_number].split(';')[0]
                 old_right = old_row[column_number].split(';')[1]
@@ -90,5 +91,11 @@ while True:
         display_freq_in_MHz_2 = float(frequency) / 1000000
         print(f"The highest decibel value among the scanned CSV files is: {display_freq_in_MHz_2} MHz at {high_dB} dB (found in {highest_file})")
 
+    #move file (remove this if doesnt work)
+    move_file = input("Do you want to move the current CSV file to the 'old_csv' folder? (y/n): ")
+    if move_file.lower() == 'y':
+        shutil.move(os.path.join(folder_path, most_recent_file), os.path.join(old_folder_path, most_recent_file))
+        print(f"Current CSV file '{most_recent_file}' moved to 'old_csv' folder.")
+
     # Wait for a specified duration before checking again
-    time.sleep(5)  # Adjust the duration as per your requirement
+    time.sleep(2)  # Adjust the duration as per your requirement
